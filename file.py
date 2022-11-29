e5c2e3bf8b9cc1cc768e465b0cf1f5169853bc3a
toxalis <toxalis@mail.ru>
2022-11-29 18:54:02 +0500

    + env

--- file.py
+++ file.py
@@ -1,3 +1,14 @@
-ytotygfpyg
-iuypiu
-yuihjkkg
+import os
+import dotenv
+
+# Команда dotenv.find_dotenv() находит файл .env    Вместо нее можно написать dotenv.load_dotenv('.env')
+dotenv.load_dotenv(dotenv.find_dotenv())
+# Находим ключ для переменной bot. Альтенативная запись bot = os.getenv('bot_token')
+# os.environ это переменные среды
+BOT_TOKEN = os.environ['BOT_TOKEN']
+
+
+print(BOT_TOKEN)
+
+
+
